c755fc3dc45eedb96e259b4facc616c2fee57950
Yaseen Saif AlShaibani <yasenshibane77@gmail.com>
2020-08-27 08:00:52 +0300

    init commit

---
+++ src/TypeIntro.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var experiencePoints : Int = 5;
+    val playerName : String = "Estragon";
+    experiencePoints+=5;
+    println(experiencePoints);
+    println(playerName);
+    var hasSteed : Boolean=false;
+    println(playerName.reversed());
+
+}
